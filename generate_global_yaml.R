198696a01434054aac85db7a225ffce510bd40ee
shalom <15121177287@163.com>
2026-05-11 17:28:12 +0800

    update

--- generate_global_yaml.R
+++ generate_global_yaml.R
@@ -27,10 +27,20 @@ global_chapters<-lapply(chapter_ymls,function(x){read_chapters(x)})
 global_chapters<-c('index.qmd',global_chapters)
 
 global_config$book$chapters<-global_chapters
-global_config$book$date<-format(Sys.time(),'%x')
+global_config$book$date <- format(Sys.time(), "%Y/%m/%d")
 
-# 写入全局 `_quarto.yml`
 write_yaml(global_config, "_quarto.yml")
 
+# 3. 暴力后处理
+lines <- readLines("_quarto.yml", encoding = "UTF-8")
 
+# 修正后的正则：
+# 匹配 date: 开头，后面跟着非引号的内容，把它包起来
+lines <- gsub("^date: ([^\".]*)", "date: \"\\1\"", lines)
+
+# 匹配 text: Copyright 开头的内容，把它包起来
+lines <- gsub("text: (Copyright[^\"]*)", "text: \"\\1\"", lines)
+
+# 4. 写回
+writeLines(lines, "_quarto.yml", useBytes = TRUE)
 
